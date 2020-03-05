ad28f8c48d8f8cefdbdc2357a5959d9fd4559ba1
KirillFilipp <kirill_filipp@mail.ru>
2020-03-05 08:08:12 +0300

    feat: add basic solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let exprStr = expr.replace(/[\s\uFEFF\xA0]/g, '');
+    let count = 0;
+    let stack = [];
+    for (let i = 0; i < exprStr.length; i++) {
+        if (exprStr[i] == '/' && exprStr[i + 1] == '0') {
+            throw new Error('TypeError: Division by zero.');
+        } else {
+        }
+    }
+    for (let i = 0; i < exprStr.length; i++) {
+        if (exprStr[i] === '(') {
+            count = count + 1;
+        } else if (exprStr[i] === ')') {
+            count = count - 1;
+        }
+    }
+    if (count !== 0) {
+        throw new Error('ExpressionError: Brackets must be paired');
+    } else {
+        return new Function('return ' + expr)();
+
+    }
 }
 
 module.exports = {
